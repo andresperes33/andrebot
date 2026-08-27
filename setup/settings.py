bd5e28abd6496215ce833f4880d061eb39b3f0f7
andresperes33 <andresperesdev@gmail.com>
2026-08-26 23:38:05 -0300

    Atualiza afiliado ML: tag=matt_word pean3412407 e matt_tool=57756886

--- setup/settings.py
+++ setup/settings.py
@@ -167,8 +167,8 @@ AWIN_API_TOKEN = os.getenv('AWIN_API_TOKEN')
 MAGALU_ID = os.getenv('MAGALU_ID', 'magazinein_1546179')
 
 # Mercado Livre Afiliados Settings
-MERCADO_LIVRE_TAG = os.getenv('MERCADO_LIVRE_TAG', 'andreindicatech')
-MERCADO_LIVRE_MATT_TOOL = os.getenv('MERCADO_LIVRE_MATT_TOOL', '13013217')
+MERCADO_LIVRE_TAG = os.getenv('MERCADO_LIVRE_TAG', 'pean3412407')
+MERCADO_LIVRE_MATT_TOOL = os.getenv('MERCADO_LIVRE_MATT_TOOL', '57756886')
 MERCADO_LIVRE_COOKIE = os.getenv('MERCADO_LIVRE_COOKIE')
 
 # Monitor Settings (Telethon)
