29096ae3a9e2a42268719fa6da1dbe5639056a45
andresperes33 <andresperesdev@gmail.com>
2026-09-05 20:57:55 -0300

    Merge migrations: 0110_fix_link_afiliado_service + 0125 (resolve migrate conflict)

---
+++ bot/migrations/0126_merge_fix_and_suportes.py
@@ -0,0 +1,8 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('bot', '0110_fix_link_afiliado_service'),
+        ('bot', '0125_reclassificar_suportes_monitor'),
+    ]
+    operations = []
