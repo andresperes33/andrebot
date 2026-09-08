02b39c85fb085c7415c0c27e8a396d216f616a08
andresperes33 <andresperesdev@gmail.com>
2026-09-08 19:57:30 -0300

    Notificacao social proof fica 50 segundos na tela

--- bot/static/bot/social-proof.js
+++ bot/static/bot/social-proof.js
@@ -75,7 +75,7 @@
         textEl.innerHTML = msg.html;
         timeEl.textContent = msg.time;
         el.classList.add('show');
-        timer = setTimeout(hideNotification, 5000);
+        timer = setTimeout(hideNotification, 50000);
     }
 
     function hideNotification() {
@@ -88,7 +88,7 @@
         interval = setInterval(function () {
             hideNotification();
             setTimeout(showNotification, 600);
-        }, 12000);
+        }, 51000);
     }
 
     if (closeBtn) {
